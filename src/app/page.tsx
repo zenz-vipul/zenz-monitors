32b4f897ad12a5f0ed420e3f4df4b22de98dd141
Vipul Jujar <vipuljujar@Sarva.local>
2024-10-07 16:29:33 +0530

    health dashboard

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Component } from "react";
+import Dashboard from "./pages/Dashboard";
+
+export default function Home() {
+  return (
+    <Dashboard />
+  );
+}
